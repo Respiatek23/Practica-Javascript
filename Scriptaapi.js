5ac5c56e946447ad3f460c5d36ad1457914c7dbb
Respiatek23 <97873151+Respiatek23@users.noreply.github.com>
2023-03-09 23:39:04 +0100

    Add files via upload

---
+++ Scriptaapi.js
@@ -0,0 +1,33 @@
+// JavaScript source code
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+const search =function(event) {
+    
+    fetch('https://api.thecatapi.com/v1/images/search',requestOptions)
+        
+        .then(response=>response.json())
+        .then(data => {
+            
+            const imagen = document.createElement('img');
+            imagen.setAttribute('src', data[0].url);
+            imagen.setAttribute('class', 'imag');
+            imagen.setAttribute('id', 'photo');
+
+            const active = document.getElementsByClassName('carousel-item active');
+
+            for (let i = 1; i < active[0].childElementCount; i++) {
+
+                if (active[0].childNodes[i].nodeName === 'IMG')
+                {
+                    const Oldimg = active[0].childNodes[i];
+                    active[0].replaceChild(imagen,Oldimg);
+                    return;
+                }
+            }
+        }
+
+        )
+        .catch(error => console.log('error', error));
+}
